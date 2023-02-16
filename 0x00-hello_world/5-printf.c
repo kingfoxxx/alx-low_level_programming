87e9cffd401b4619722e2d342034eb08c5899608
kingfoxxx <brightally6@gmail.com>
2023-02-16 06:46:27 -0800

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line using the printf function
- * Return: 0 (Sucess)
+ * main - prints "with proper grammar, but the outcome is a piece of art"
+ * Return:Always 0.
+ *
  */
 int main(void)
 {
