8b7719a10b15303f965c0f29d384854eec963407
kingfoxxx <brightally6@gmail.com>
2023-03-02 08:55:16 -0800

    inf

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,24 +2,28 @@
 
 
 /**
- * print_number - Prints an integer.
- * @n: The integer to be printed.
+ * print_number - Print numbers chars
+ * @n: integer params
  *
+ *Return: 0
  */
 
 void print_number(int n)
 
 {
 
-	unsigned int num = n;
+	unsigned int n1;
 
+	n1 = n;
 	if (n < 0)
 	{
 		_putchar('_');
-		num = -num;
+		n1 = -n;
 	}
 
-	if ((num / 10) > 0)
-		print_number(num / 10);
-	_putchar((num % 10) + '0');
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
 }
